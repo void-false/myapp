b60f435db2fed3125383b911a22f7087441617ed
Void False <void.false@gmail.com>
2017-10-06 14:58:52 +0000

    moved all to new pago repo

---
+++ avx/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Avxhome</title>
+    <meta charset="UTF-8">
+    <base href="http://avxhome.in" target="_blank">
+    <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+    <style>
+        body {
+            font-family:"Helvetica";
+        }
+        div { text-align:center; }
+        .title-link {
+             display: none;
+             color:#a52a2a; 
+             font-size:16px;
+             font-weight:500;
+             text-decoration:none;
+             text-transform:none;
+        }
+        #pgdn {
+            box-sizing: border-box;
+            font-size: 40px;
+            width: 50px;
+            height: 50px;
+            background-color: cornsilk;
+            border: 1px solid black;
+            text-align: center;
+            position: fixed;
+            left: 60%;
+            bottom: 10%;
+        }
+        img {
+            width: 200px;
+        }
+    </style>
+    <script>
+        function pageScroll() {
+            window.scrollBy(0, window.innerHeight);
+        }
+    </script>
+</head>
+<body>
+    <div id="pgdn" onclick="pageScroll()">&mapstodown;</div>
+<?php 
+ob_implicit_flush(true);
+require 'simple_html_dom.php';
+
+
+for($i = 1; $i <= 69; $i++) {
+    $html = file_get_html("http://avxhome.in/pages/" . $i);
+    foreach($html->find('script') as $crap) { $crap->innertext=""; }
+    
+    foreach($html->find('div[class=article]') as $element) {
+        echo $element;
+    }
+    //for($k = 0; $k < 40000; $k++) { echo ' '; }
+    sleep(2);
+}
+
+
+//<div class="col-md-12 article">
+?>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,87 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BTC-E!</title>
-	<style>
-		body {
-			display: none;
-		}
-		.now { text-decoration: underline; }
-		#ltc_count { width: 75px; }
-	</style>
-  </head>
-  <body>
-    <h1>BTC-e Lives!</h1>
-	
-	<div>Last Updated: <span class="now">00:00:00<span></span></div>
-	<div class="result"><p>Loading data...</p></div>
-	
-	<div class="sc">
-		<p></p>
-	</div>
-	
-	<div class="etc_mining"></div>
-	<div class="sc_mining"></div>
+<html>
+    <head>
+        <title>Reaction Tester</title>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device=width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
     
-    <!-- jQuery  -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script>
-	/*global $*/
-	$("body").fadeIn("slow");
-	
-	var getTime = function() {
-		var now = new Date();
-		var strNow = ((now.getHours() < 10 ? "0"+now.getHours() : now.getHours()) + ":" +
-					  (now.getMinutes() < 10 ? "0"+now.getMinutes() : now.getMinutes()) + ":" +
-					  (now.getSeconds() < 10 ? "0"+now.getSeconds() : now.getSeconds()));
-		$(".now").html(strNow);
-	}
-	
-	var getData = function() {
-		$.ajax({
-			url: 'getbtc.php',
-			type: 'GET',
-			dataType: 'json',
-			success: function(data) {
-				//data - response from server
-				$(".result").html("<p>High: " + data.btc_high + "<br>" 
-										   + "Low: " + data.btc_low + "<br>"
-										   + "<strong>Last: " + data.btc_last + "</strong></p>"
-										   
-										   + "<p>High: " + data.ltc_high + "<br>" 
-										   + "Low: " + data.ltc_low + "<br>"
-										   + "<strong>Last: " + data.ltc_last + "</strong></p>"
-										   
-										   + "<p>High: " + data.ltcbtc_high + "<br>" 
-										   + "Low: " + data.ltcbtc_low + "<br>"
-										   + "<strong>Last: " + data.ltcbtc_last + "</p>");
-										   
-				$(".sc").html("<p>High: " + data.sc_high + "<br>" 
-										   + "Low: " + data.sc_low + "<br>"
-										   + "<strong>Last: " + data.sc_last + "</strong></p>");
-			
-				$(".now").html(data.time_now);
-				
-				document.title = Math.trunc(data.btc_last) + "/" + Math.trunc(data.ltc_last) + "/" + data.ltcbtc_last.slice(0,7) + "/" + data.sc_last.slice(-4);
-			}
-		});
-	};
-	
-	getTime();
-	getData();
-	
-	//setInterval(function() {
-	//	getTime();
-	//}, 1000);
-	
-	//https://btc-e.com/api/3/ticker/btc_usd
-	setInterval(function() { 
-		getData();	
-	}, 2000);
+    <body>
+        <p>Reaction Time: <span id="result"></span></p>
+        <div id="container">
+            <div id="box"></div>
+        </div>
+        <script src="script.js"></script>
+    </body>
 
-	</script>
-  </body>
+
+
+
 </html>
